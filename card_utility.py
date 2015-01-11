6de7f2be128e62870b1a6767c8d4c5ee11120f6d
Shannon Tsaltas <shannontsaltas@gmail.com>
2015-01-11 16:25:06 -0600

    initial commit

---
+++ card_utility.py
@@ -0,0 +1,13 @@
+def generate_html(template, data):
+	pass
+
+def generate_js(template, data):
+	pass
+
+if __name__ == "__main__":
+	html_template = ""
+	javascript_template = ""
+	json_data = ""
+
+	generate_html(html_template, json_data)
+	generate_js(html_template, json_data)
---
+++ test_card_utility.py
@@ -0,0 +1,12 @@
+import unittest
+
+from card_utility import generate_html, generate_js
+
+class CardUtilityTests(unittest.TestCase):
+	def test_generate_html(self):
+		pass
+	def test_generate_html(self):
+		pass
+
+if __name__ == "__main__":
+    unittest.main()
